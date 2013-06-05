58ee3d8b13a1934cb400c460fcaf53cb4d5339e4
Darrick J. Wong <djwong@blackbox.djwong.org>
2013-06-04 22:17:48 -0700

    Tweak some of the event calendar bits...

--- next/data/big_events.js
+++ next/data/big_events.js
@@ -2,5 +2,5 @@
 {"start": "2013/3/9 19:00", "type": "ball", "name": "Portland Ball", "location": "Christ Episcopal Church, Lake Oswego", "crib": "portland_ball_2013.txt"},
 {"start": "2013/5/3 19:00", "type": "workshop", "name": "Youth Weekend West", "location": "The Academy, Vancouver, Wash.", "crib": "yww2013_ceilidh.txt", "url": "http://www.youthweekendwest.com/"},
 {"start": "2013/5/4 18:00", "type": "ball", "name": "Dinner Dance", "location": "St. Luke's, Vancouver, Wash."},
-{"start": "2014/3/8 9:00", "type": "workshop", "name": "Portland Workshop", "location": "Christ Episcopal Church, Lake Oswego", "url": "workshop.html"},
-{"start": "2014/3/8 19:00", "ready": false, "name": "Portland Ball", "type": "ball", "location": "Christ Episcopal Church, Lake Oswego", "url": "workshop.html"},
+{"start": "2014/3/8 9:00", "type": "workshop", "name": "Portland Workshop", "location": "TBD", "url": "workshop.html"},
+{"start": "2014/3/8 19:00", "ready": false, "name": "Portland Ball", "type": "ball", "location": "TBD", "url": "workshop.html"},
