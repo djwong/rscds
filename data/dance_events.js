f8660553b9d1f9474fefb81ababd677ce8261220
Darrick J. Wong <djwong@blackbox.djwong.org>
2013-06-24 16:48:26 -0700

    Fix all the broken links resulting from moving files around.

--- data/dance_events.js
+++ data/dance_events.js
@@ -1,4 +1,4 @@
-{"start": "2013/07/07 12:00", "type": "dance", "name": "Sumptuous Summer Picnic", "location": "Clark Co., Wash.", "url": "/picnicflyer13.pdf"},
+{"start": "2013/07/07 12:00", "type": "dance", "name": "Sumptuous Summer Picnic", "location": "Clark Co., Wash.", "url": "files/picnicflyer13.pdf"},
 {"start": "2013/07/20 09:00", "type": "dance", "name": "Portland Highland Games", "location": "Mt. Hood Community College, Gresham, Ore.", "url": "http://www.phga.org/"},
 {"start": "2013/10/12 19:30", "type": "dance", "name": "Portland Dance", "location": "Waluga Lodge"},
 {"start": "2013/10/26 19:00", "type": "dance", "name": "Fall Dance", "location": "Vancouver, Wash."},
