b8fb6b34bd6a930cbaa844ad5458be263d7c87cf
Darrick J. Wong <djwong@djwong.org>
2013-11-19 12:10:19 -0800

    Add some more events to the calendar.

--- data/dance_events.js
+++ data/dance_events.js
@@ -7,4 +7,4 @@
 {"start": "2013/10/26 19:00", "type": "dance", "name": "Fall Dance", "location": "Vancouver, Wash.", "url": "http://www.rscds-swws.org/fallflyer13.pdf", "crib": "swws_oct2013.txt"},
 {"start": "2013/11/09 19:30", "type": "dance", "name": "Portland Dance", "location": "Waluga Lodge", "crib": "portland_nov2013.txt", "url": "/party.html"},
 {"start": "2013/12/14 19:30", "type": "dance", "name": "Portland Dance", "location": "Waluga Lodge", "crib": "portland_dec2013.txt", "url": "/party.html"},
-{"start": "2013/12/28 19:30", "type": "dance", "name": "Betwixt and Between Dance", "location": "Vancouver, Wash."},
+{"start": "2013/12/28 19:30", "type": "dance", "name": "Betwixt and Between Dance", "location": "Vancouver, Wash.", "url": "files/betwixt_2013.pdf"},
