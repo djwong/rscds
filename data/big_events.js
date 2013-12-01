bbab18ae29d7d1d31f87a64103d2ed7a56009307
Darrick J. Wong <djwong@larch.djwong.org>
2013-11-30 22:42:11 -0800

    Add the TAC ball program.

--- data/big_events.js
+++ data/big_events.js
@@ -1,4 +1,5 @@
 {"start": "2012/03/10 19:30", "type": "ball", "name": "Portland Ball", "location": "Norse Hall, Portland", "crib": "portland_ball_2012.txt", "url": "workshop.html"},
+{"start": "2012/08/04 20:00", "type": "ball", "name": "TAC Ball", "location": "Portland State University", "crib": "tac_ball_2012.txt"},
 {"start": "2013/3/9 9:00", "type": "workshop", "name": "Portland Workshop", "location": "Christ Episcopal Church, Lake Oswego", "url": "workshop.html"},
 {"start": "2013/3/9 19:00", "type": "ball", "name": "Portland Ball", "location": "Christ Episcopal Church, Lake Oswego", "crib": "portland_ball_2013.txt"},
 {"start": "2013/5/3 19:00", "type": "workshop", "name": "Youth Weekend West", "location": "The Academy, Vancouver, Wash.", "crib": "yww2013_ceilidh.txt", "url": "http://www.youthweekendwest.com/"},
