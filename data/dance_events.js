e14d1f633d38f5b36402e6d99f1abfda71c5b4ce
Darrick J. Wong <djwong@alder.djwong.org>
2013-12-17 11:04:30 -0800

    Import Feb 2014 dance program.

--- data/dance_events.js
+++ data/dance_events.js
@@ -10,3 +10,4 @@
 {"start": "2013/12/14 19:30", "type": "dance", "name": "Portland Dance", "location": "Waluga Lodge", "crib": "portland_dec2013.txt", "url": "/party.html"},
 {"start": "2013/12/28 19:30", "type": "dance", "name": "Betwixt and Between Dance", "location": "Vancouver, Wash.", "url": "files/betwixt_2013.pdf", "crib": "betwixt_2013.txt"},
 {"start": "2014/01/11 19:30", "type": "dance", "name": "Portland Dance", "location": "Waluga Lodge", "crib": "portland_jan2014.txt"},
+{"start": "2014/02/08 19:30", "type": "dance", "name": "Portland Dance", "location": "Waluga Lodge", "crib": "portland_feb2014.txt"},
