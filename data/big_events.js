0939a7d7c193ea703ab2ea7ed79062747c213927
Darrick J. Wong <djwong@djwong.org>
2013-12-17 12:10:33 -0800

    Correct the time of the Dinner Dance.

--- data/big_events.js
+++ data/big_events.js
@@ -6,4 +6,4 @@
 {"start": "2013/5/4 18:00", "type": "ball", "name": "Dinner Dance", "location": "St. Luke’s, Vancouver, Wash.", "crib": "dinner_dance_2013.txt", "url": "http://www.rscds-swws.org/2013-dinnerdance-among-stars-flyer.pdf"},
 {"start": "2014/3/8 9:00", "type": "workshop", "name": "Portland Workshop", "location": "Lake Oswego, Ore.", "url": "workshop.html"},
 {"start": "2014/3/8 19:00", "ready": true, "name": "Portland Ball", "type": "ball", "location": "Lake Oswego, Ore.", "url": "workshop.html", "crib": "portland_ball_2014.txt"},
-{"start": "2014/4/26 19:00", "type": "ball", "name": "Dinner Dance", "location": "Vancouver, Wash."},
+{"start": "2014/4/26 18:00", "type": "ball", "name": "Dinner Dance", "location": "Vancouver, Wash."},
