268ced2d55359030ef50b323a1697a0ff973cf8c
Darrick J. Wong <djwong@djwong.org>
2014-01-09 23:37:34 -0800

    Note the new GG.

--- data/news_events.js
+++ data/news_events.js
@@ -1,3 +1,4 @@
+{"start": "2014/01/01 00:01", "type": "news", "name": "January 2014 Ghillie Gazette Published", "details": "The <a href=\"http://portlandscottishdancers.org/gazette/2014-01.html\">winter issue</a> is now available."},
 {"start": "2013/06/24 17:30", "type": "news", "name": "New Website Available", "details": "Darrick has made the new website available at last!"},
 {"start": "2013/03/09 23:59", "type": "news", "name": "Successful ball!", "details": "Thank you all who made the 34th ball such a huge success!"},
 {"start": "2013/05/05 11:30", "type": "news", "name": "Youth Weekend a Blast!", "details": "We greatly enjoyed hosting everyone for Youth Weekend.  Thank you all for participating!"},
