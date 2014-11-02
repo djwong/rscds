d60119fb7eb21aad3ee36f696bff25b1ce48b020
Darrick J. Wong <djwong@larch.djwong.org>
2014-11-02 13:12:07 -0800

    Travel calendar updates.

--- data/class_events.js
+++ data/class_events.js
@@ -192,7 +192,6 @@
 {"start": "2014/10/10 19:00", "type": "class", "name": "Friday Class", "location": "Columbia Dance Center, Vancouver, Wash."},
 {"start": "2014/10/24 19:00", "type": "class", "name": "Friday Class", "location": "Columbia Dance Center, Vancouver, Wash."},
 {"start": "2014/10/31 19:00", "type": "class", "name": "Friday Class", "location": "Columbia Dance Center, Vancouver, Wash."},
-{"start": "2014/11/07 19:00", "type": "class", "name": "Friday Class", "location": "Columbia Dance Center, Vancouver, Wash."},
 {"start": "2014/11/14 19:00", "type": "class", "name": "Friday Class", "location": "Columbia Dance Center, Vancouver, Wash."},
 {"start": "2014/11/21 19:00", "type": "class", "name": "Friday Class", "location": "Columbia Dance Center, Vancouver, Wash."},
 {"start": "2014/11/28 19:00", "type": "class", "name": "Friday Class", "location": "Columbia Dance Center, Vancouver, Wash."},
--- data/dance_events.js
+++ data/dance_events.js
@@ -8,7 +8,7 @@
 {"start": "2013/11/09 19:30", "type": "dance", "name": "Portland Dance", "location": "Waluga Lodge, Lake Oswego, Ore.", "crib": "portland_nov2013.txt", "url": "/party.html"},
 {"start": "2013/11/29 15:00", "type": "dance", "name": "Werk Off Yer Turkey Dance", "location": "Vancouver, Wash.", "url": "/files/turkey_2013.pdf"},
 {"start": "2013/12/14 19:30", "type": "dance", "name": "Portland Dance", "location": "Waluga Lodge, Lake Oswego, Ore.", "crib": "portland_dec2013.txt", "url": "/party.html"},
-{"start": "2013/12/28 19:30", "type": "dance", "name": "Betwixt and Between Dance", "location": "Vancouver, Wash.", "url": "files/betwixt_2013.pdf", "crib": "betwixt_2013.txt"},
+{"start": "2013/12/28 14:00", "type": "dance", "name": "Betwixt and Between Dance", "location": "Vancouver, Wash.", "url": "files/betwixt_2013.pdf", "crib": "betwixt_2013.txt"},
 {"start": "2014/01/11 19:30", "type": "dance", "name": "Portland Dance", "location": "Waluga Lodge, Lake Oswego, Ore.", "crib": "portland_jan2014.txt", "url": "/party.html"},
 {"start": "2014/02/08 19:30", "type": "dance", "name": "Portland Dance CANCELLED", "location": "CANCELLED", "crib": "portland_feb2014.txt", "url": "/party.html"},
 {"start": "2014/04/12 19:30", "type": "dance", "name": "Portland Dance", "location": "Waluga Lodge, Lake Oswego, Ore.", "url": "/party.html", "crib": "portland_apr2014.txt"},
