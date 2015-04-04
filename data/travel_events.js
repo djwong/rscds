e37ae001144de3774bf71c7ed046bb88134845c6
Darrick J. Wong <djwong@djwong.org>
2015-04-04 15:25:03 -0700

    Add the Truckee ball.

--- data/travel_events.js
+++ data/travel_events.js
@@ -67,5 +67,6 @@
 {"start": "2015/03/28 08:00", "type": "workshop", "local": false, "name": "Vancouver Is. March Workshop", "location": "Vancouver Is.", "url": "http://viscds.ca/workshop-and-spring-social/"},
 {"start": "2015/05/01 00:00", "type": "workshop", "local": false, "name": "Youth Weekend Ceilidh", "location": "Victoria, B.C.", "url": "http://youthweekendwest.com/"},
 {"start": "2015/05/02 00:00", "type": "ball", "local": false, "name": "Youth Weekend Ball", "location": "White Eagle Polish Hall, Victoria, B.C.", "url": "http://youthweekendwest.com/", "crib": "yww2015_ball.txt"},
+{"start": "2015/06/27 13:00", "type": "ball", "local": false, "name": "Silver State SCD Dance Party", "location": "Truckee Community Arts Center, Truckee, Calif.", "url": "http://silverstatescd.wix.com/sscds#!truckee-workshop/cee5"},
 {"start": "2015/11/13 00:00", "type": "ball", "local": false, "name": "Asilomar Ceilidh", "location": "Pacific Grove, Calif.", "url": "http://asilomar.rscds-sf.org/"},
 {"start": "2015/11/14 00:00", "type": "ball", "local": false, "name": "Asilomar Ball", "location": "Pacific Grove, Calif.", "url": "http://asilomar.rscds-sf.org/"},
