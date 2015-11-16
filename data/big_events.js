bfd9c8884b804f64915ebbe5ae637a2a8169d565
Darrick J. Wong <djwong@djwong.org>
2015-11-15 22:40:26 -0800

    Record the dinner dance date.

--- data/big_events.js
+++ data/big_events.js
@@ -11,3 +11,4 @@
 {"start": "2015/3/14 19:00", "type": "ball", "name": "Portland Pi Ball", "location": "Christ Episcopal Church, Lake Oswego", "crib": "portland_ball_2015.txt", "url": "files/workshop_registration_2015.pdf"},
 {"start": "2015/4/25 18:00", "type": "ball", "name": "Dinner Dance", "location": "Clark County Sq. Dance Center, Vancouver, Wash.", "crib": "dinner_dance_2015.txt", "local": true, "url": "http://www.rscds-swws.org/2015-dinnerdance.pdf", "ready": true},
 {"start": "2016/03/12 19:00", "type": "ball", "name": "Portland Ball", "location": "Portland"},
+{"start": "2016/04/23 00:00", "type": "ball", "name": "Dinner Dance", "location": "Vancouver, Wash.", "local": true, "ready": true},
