4125317a0dd3eda7c3e25e09586181d004e2ef25
Darrick J. Wong <djwong@djwong.org>
2016-03-07 21:46:45 -0800

    Note the new GG in the news.

--- data/news_events.js
+++ data/news_events.js
@@ -1,3 +1,4 @@
+{"start": "2016/03/07 21:46", "type": "news", "name": "March Ghillie Gazette", "details": "The March 2016 edition of the <a href=\"/gazette/2016-03.html\">Ghillie Gazette</a> is out."},
 {"start": "2016/01/14 10:35", "type": "news", "name": "Ball Registration Open", "details": "You can now <a href=\"/workshop.html\">register</a> for the 37th annual Workshop and Ball!"},
 {"start": "2016/01/06 00:35", "type": "news", "name": "January Ghillie Gazette", "details": "The January 2016 edition of the <a href=\"/gazette/2016-01.html\">Ghillie Gazette</a> is out."},
 {"start": "2015/11/02 16:45", "type": "news", "name": "November Ghillie Gazette", "details": "The November 2015 edition of the <a href=\"/gazette/2015-11.html\">Ghillie Gazette</a> is out."},
