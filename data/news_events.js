9e3ea7688509352694b0084f90f8fece881cde5f
Darrick J. Wong <djwong@djwong.org>
2017-03-17 15:53:44 -0700

    Add news item for 3/17 GG

--- data/news_events.js
+++ data/news_events.js
@@ -1,3 +1,4 @@
+{"start": "2017/03/17 22:09", "type": "news", "name": "March Ghillie Gazette", "details": "The March 2017 edition of the <a href=\"/gazette/2017-03.html\">Ghillie Gazette</a> is out."},
 {"start": "2017/02/15 22:09", "type": "news", "name": "January Ghillie Gazette", "details": "The January 2017 edition of the <a href=\"/gazette/2017-01.html\">Ghillie Gazette</a> is out."},
 {"start": "2016/05/02 14:00", "type": "news", "name": "May Ghillie Gazette", "details": "The May 2016 edition of the <a href=\"/gazette/2016-05.html\">Ghillie Gazette</a> is out."},
 {"start": "2016/03/07 21:46", "type": "news", "name": "March Ghillie Gazette", "details": "The March 2016 edition of the <a href=\"/gazette/2016-03.html\">Ghillie Gazette</a> is out."},
