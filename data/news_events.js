829f152740ced389e39289f8032c5b71fd2d2716
Darrick J. Wong <djwong@djwong.org>
2019-01-07 18:56:26 -0800

    news event for new gg

--- data/news_events.js
+++ data/news_events.js
@@ -1,3 +1,4 @@
+{"start": "2019/01/07 18:55", "type": "news", "name": "January Ghillie Gazette", "details": "The January/February 2019 edition of the <a href=\"/gazette/2019-01.html\">Ghillie Gazette</a> is out."},
 {"start": "2018/11/03 11:55", "type": "news", "name": "November Ghillie Gazette", "details": "The November/December 2018 edition of the <a href=\"/gazette/2018-11.html\">Ghillie Gazette</a> is out."},
 {"start": "2018/09/18 07:07", "type": "news", "name": "September Ghillie Gazette", "details": "The September/October 2018 edition of the <a href=\"/gazette/2018-9.html\">Ghillie Gazette</a> is out."},
 {"start": "2018/07/06 07:07", "type": "news", "name": "July Ghillie Gazette", "details": "The July/August 2018 edition of the <a href=\"/gazette/2018-7.html\">Ghillie Gazette</a> is out."},
